0569a333d3fa4fcdd02e07b3865f7c45d716555a
aldrinburgos <burgos.aldrin@gmail.com>
2022-03-06 20:51:17 -0600

    add html routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const express = require("express");
 const PORT = process.env.PORT || 3001;
@@ -9,8 +10,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(3001, () => {
   console.log(`Server is now live on port ${PORT}!`);
