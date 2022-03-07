88de4f3dfcaaeab74d6a2c83cef19f424a87efb7
aldrinburgos <burgos.aldrin@gmail.com>
2022-03-06 22:26:54 -0600

    add delete functionality

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,18 +1,21 @@
 const router = require("express").Router();
 
-const { findById, validateNote, createNote } = require("../../lib/notes");
+const {
+  findById,
+  validateNote,
+  createNote,
+  deleteNote,
+} = require("../../lib/notes");
 
 const notes = require("../../db/db.json");
 
 router.get("/notes", (req, res) => {
   let results = notes;
-  console.log(results);
   res.json(results);
 });
 
 router.get("/notes/:id", (req, res) => {
   let result = findById(req.params.id, notes);
-  console.log(req.params.id);
   if (result) {
     res.json(result);
   } else {
@@ -22,7 +25,13 @@ router.get("/notes/:id", (req, res) => {
 
 router.post("/notes", (req, res) => {
   //set id
-  req.body.id = notes.length.toString();
+  if (!notes.length) {
+    req.body.id = "0";
+  } else {
+    //get last element of array's id then add 1
+    const id = (Number(notes.slice(-1)[0].id) + 1).toString();
+    req.body.id = id;
+  }
 
   if (!validateNote(req.body)) {
     console.log("Note is not propery formatted!");
@@ -30,6 +39,12 @@ router.post("/notes", (req, res) => {
     const note = createNote(req.body, notes);
     res.json(note);
   }
+});
+
+router.delete("/notes/:id", (req, res) => {
+  const id = req.params.id;
+  const deletedNote = deleteNote(id, notes);
+  res.json(deletedNote);
 });
 
 module.exports = router;
